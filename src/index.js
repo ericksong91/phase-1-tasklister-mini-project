812a2f102bfcf9897445925ea250766ced1b24db
Eric Song <ericksong91@gmail.com>
2022-08-09 15:17:28 -0400

    Basic functionality, delete, add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      makeToDoList(e.target.task_toDo.value)
+      form.reset()
+  })
 });
+
+function makeToDoList(todo){
+    let li = document.createElement("li")
+    let delbtn = document.createElement("button")
+    delbtn.textContent = 'x'
+    delbtn.addEventListener('click', handleDel)
+    li.textContent = `${todo} `
+    li.appendChild(delbtn)
+    document.querySelector('#tasks').appendChild(li)
+}
+
+function handleDel(e){
+    e.target.parentNode.remove()
+}
